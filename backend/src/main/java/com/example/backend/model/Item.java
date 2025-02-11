0c09f4e996698dff32a401c3b108c552f8267e48
callmax02 <callmax_02@proton.me>
2025-02-11 16:49:24 -0500

    1.03 - Fixed CORS error and removed unique constraints on assetTag and serial fields

--- backend/src/main/java/com/example/backend/model/Item.java
+++ backend/src/main/java/com/example/backend/model/Item.java
@@ -21,11 +21,11 @@ public class Item {
     private String department;
 
     @NotBlank(message = "Asset Tag is required")
-    @Column(unique = true)
+    // @Column(unique = true)
     private String assetTag;
 
     @NotBlank(message = "Serial is required")
-    @Column(unique = true)
+    // @Column(unique = true)
     private String serial;
 
     @NotBlank(message = "Model is required")
